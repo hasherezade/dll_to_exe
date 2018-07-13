2c0038ae82747d947b06f5c6f3ad2b804f3c291e
hasherezade <hasherezade@gmail.com>
2018-07-13 21:00:06 +0200

    [INIT] Template setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# DLL_to_EXE
+Converts a DLL to a ready-to-use EXE.
---
+++ dll_to_exe/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <windows.h>
+
+#include "peconv.h"
+
+int main(int argc, char *argv[])
+{
+    system("pause");
+	return 0;
+}
